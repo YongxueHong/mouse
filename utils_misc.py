da7d423da78ca89b1a309c9ebad133d5ffc5e9c1
Xianwang <xianwang@redhat.com>
2018-05-25 15:58:38 +0800

    JIRA[ELPPCKVM-960]:[Framework]Add "id" to qmp command 1.add "id" for qmp command modify monitor.py and utils_misc.py 2.fix bug for stable abi {"return": {"status": "failed", "error-desc": "Failed to connect socket: Connection refused"}, "id": "rgqiiB45"}
    Signed-off-by: Xianwang <xianwang@redhat.com>

--- utils_misc.py
+++ utils_misc.py
@@ -1,4 +1,6 @@
 import time
+import random
+import string
 
 def wait_for_output(func, timeout, first=0.0, step=1.0):
 
@@ -43,3 +45,30 @@ def py2_to_srt(unicode_or_str):
     else:
         value = unicode_or_str
     return value
+
+def generate_random_string(length, ignore_str=string.punctuation,
+                           convert_str=""):
+    """
+    Return a random string using alphanumeric characters.
+
+    :param length: Length of the string that will be generated.
+    :param ignore_str: Characters that will not include in generated string.
+    :param convert_str: Characters that need to be escaped (prepend "\\").
+
+    :return: The generated random string.
+    """
+    r = random.SystemRandom()
+    sr = ""
+    chars = string.ascii_letters + string.digits + string.punctuation
+    if not ignore_str:
+        ignore_str = ""
+    for i in ignore_str:
+        chars = chars.replace(i, "")
+
+    while length > 0:
+        tmp = r.choice(chars)
+        if convert_str and (tmp in convert_str):
+            tmp = "\\%s" % tmp
+        sr += tmp
+        length -= 1
+    return sr
