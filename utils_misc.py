c54ce8fe07f77b86f6d7f5864ebfda9286444bc0
YongxueHong <yhong@redhat.com>
2018-05-31 08:09:57 +0800

    ELPPCKVM-966:[framework] update utils_misc.py and utils_guest.py
    add a function wait_for_output and delete two class GuestSessionOld and GuestSessionOldV2.
    
    Signed-off-by: Yongxue Hong <yhong@redhat.com>

--- utils_misc.py
+++ utils_misc.py
@@ -1,17 +1,13 @@
 import time
 
 def wait_for_output(func, timeout, first=0.0, step=1.0):
-
-    start_time = time.time()
     end_time = time.time() + float(timeout)
-
     time.sleep(first)
 
     while time.time() < end_time:
         output = func()
         if output:
             return output
-
         time.sleep(step)
 
     return None
